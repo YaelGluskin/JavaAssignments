8ac9a41926e303ccaf442a41286c30e13c1fde5d
Yael Gluskin <yaelgluskin@gmail.com>
2024-01-19 14:53:37 +0200

    lanch project

---
+++ src/game/Game.java
@@ -0,0 +1,15 @@
+package game;
+
+public class Game {
+
+	public Game() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helo");
+
+	}
+
+}
